31785bfea5674209d8bc7b176670fd873b094e43
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-12 15:12:31 -0300

    FEAT: add react alert setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-import './index.scss';
-import reportWebVitals from './reportWebVitals';
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offeset: "30px",
+  transition: transitions.SCALE,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>
 );
 
