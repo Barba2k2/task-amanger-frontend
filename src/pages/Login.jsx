ffb318916fb146e2a8473cd07b710ffc0bce4eb7
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-12 22:40:40 -0300

    FEAT: Add login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,17 @@
+import CustomButton from "../components/CustomButton";
+
+import "./Login.scss";
+import logo from "../assets/images/logo-2.png";
+
 const Login = () => {
-  return <></>;
+  return (
+    <div className="login-container">
+      <img src={logo} alt="logo" />
+      <div className="button-container">
+        <CustomButton>Entrar</CustomButton>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
